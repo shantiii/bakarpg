c2323739e20adf787525a968003a65dd0fc24076
shanti <sh3llc4t@gmail.com>
2014-11-03 19:56:45 -0500

    Add redis log

--- websocket-server.rb
+++ websocket-server.rb
@@ -5,6 +5,7 @@ require 'json'
 require 'thin'
 require 'yaml'
 require 'cgi' #escaping HTML
+require 'redis'
 
 config_file = ARGV[0] || File.join(File.dirname(__FILE__), "configuration.yml")
 config = YAML.load_file(config_file)
@@ -20,44 +21,50 @@ def log(msg)
 end
 
 def error_rsp(msg)
-  { type:"error", message:msg }.to_json
+  { type:"error", message:msg }
 end
 
 def chat(nick, msg)
-  { type:"chat", nick:nick, message:msg }.to_json
+  { type:"chat", nick:nick, message:msg }
 end
 
 def ooc(nick, msg)
-  { type:"ooc", nick:nick, message:msg }.to_json
+  { type:"ooc", nick:nick, message:msg }
 end
 
 def emote(nick, msg)
-  { type:"emote", nick:nick, message:msg }.to_json
+  { type:"emote", nick:nick, message:msg }
 end
 
 def renamed(old_nick, new_nick)
-  { type:"rename", old:old_nick, new:new_nick }.to_json
+  { type:"rename", old:old_nick, new:new_nick }
 end
 
 def id(granted_nick)
-  { type:"id", nick:granted_nick }.to_json
+  { type:"id", nick:granted_nick }
 end
 
 def joined(joined_nick)
-  { type:"join", nick:joined_nick }.to_json
+  { type:"join", nick:joined_nick }
 end
 
 def leave(left_nick)
-  { type:"leave", nick:left_nick }.to_json
+  { type:"leave", nick:left_nick }
 end
 
 def userlist(nicks)
-  {type:"userlist", nicks:nicks}.to_json
+  {type:"userlist", nicks:nicks}
 end
 
 EventMachine.run do
   @channel = EventMachine::Channel.new
   @names = {}
+  @redis = Redis.new
+
+  def push_msg(msg_obj)
+    msg_obj[:msgid] = @redis.rpush("campaign:test.log", msg_obj)
+    @channel.push(msg_obj.to_json)
+  end
 
   EventMachine::WebSocket.start(host: '0.0.0.0', port: config['websocket_port'], debug: true) do |socket|
     socket.onopen do |handshake|
@@ -66,44 +73,48 @@ EventMachine.run do
         socket.send msg
       end
       @names[sid] = "user#{sid}"
-      socket.send id(@names[sid])
-      socket.send userlist(@names.values)
-      @channel.push joined(@names[sid])
+      socket.send id(@names[sid]).to_json
+      socket.send userlist(@names.values).to_json
+      push_msg joined(@names[sid])
+
       socket.onmessage do |jsonmsg|
         begin
           msg = JSON.parse(jsonmsg)
-        socket.send_text error_rsp("Not a valid request.") if msg.nil?
-        msg['message'] = CGI.escapeHTML(msg['message']) unless msg['message'].nil?
-        response = case(msg['type'])
-                   when 'nick'
-                     potential_nick = CGI.escapeHTML(msg['nick'])
-                     raise ApplicationError.new("Nick '#{potential_nick}' is already taken.") if @names.values.include? potential_nick
-                     oldname = @names[sid]
-                     @names[sid] = potential_nick
-                     renamed(oldname, @names[sid])
-                   when 'chat' then chat(@names[sid], msg['message'])
-                   when 'emote' then emote(@names[sid], msg['message'])
-                   when 'ooc' then ooc(@names[sid], msg['message'])
-                   when 'userlist' then userlist(@names.values)
-                   else raise ApplicationError.new("Request does not contain a valid message.")
-                   end
-        @channel.push(response)
+          socket.send_text error_rsp("Not a valid request.").to_json if msg.nil?
+          msg['message'] = CGI.escapeHTML(msg['message']) unless msg['message'].nil?
+          if (msg['type'] == 'userlist')
+            socket.send userlist(@names.values).to_json
+            next
+          end
+          response = case(msg['type'])
+                     when 'nick'
+                       potential_nick = CGI.escapeHTML(msg['nick'])
+                       raise ApplicationError.new("Nick '#{potential_nick}' is already taken.") if @names.values.include? potential_nick
+                       oldname = @names[sid]
+                       @names[sid] = potential_nick
+                       renamed(oldname, @names[sid])
+                     when 'chat' then chat(@names[sid], msg['message'])
+                     when 'emote' then emote(@names[sid], msg['message'])
+                     when 'ooc' then ooc(@names[sid], msg['message'])
+                     else raise ApplicationError.new("Request does not contain a valid message.")
+                     end
+          push_msg(response)
         rescue ApplicationError => e
-          socket.send_text error_rsp(e.message);
+          socket.send_text error_rsp(e.message).to_json;
         rescue JSON::ParserError
-          socket.send_text error_rsp("Request does not contain valid JSON.")
+          socket.send_text error_rsp("Request does not contain valid JSON.").to_json
         end
       end
+
       socket.onclose do
-        puts "sads!"
         @channel.unsubscribe(sid)
-        @channel.push leave(@names[sid])
+        push_msg leave(@names[sid])
         @names.delete sid
       end
+
       socket.onerror do |error|
-        puts "sads"
         @channel.unsubscribe(sid)
-        @channel.push leave(@names[sid])
+        push_msg leave(@names[sid])
         @names.delete sid
         if (error.kind_of?(EventMachine::WebSocket::WebSocketError))
           log(error)
